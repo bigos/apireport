0232ef063479b1d6c3d4ae44e876fe6df3dcd575
Jacek Podkanski <ruby.object@googlemail.com>
2020-08-17 00:07:48 +0100

    slight progress

--- tasks/server/start.clj
+++ tasks/server/start.clj
@@ -1,4 +1,5 @@
-(ns server.start)
+(ns server.start
+  (:require [ring.adapter.jetty]))
 
 ;;; I want to run: clj -A:tasks
 ;;; to start the server
@@ -9,4 +10,8 @@
   (println "task for starting server on port 3000")
   (use 'ring.adapter.jetty)
   (use 'apireport.core)
-  (run-jetty apireport.core/handler {:port 3000 :join? false}))
+  (run-jetty apireport.core/handler {:port 3000 :join? false})
+  )
+
+;;; i get the error
+;; Unable to resolve symbol: run-jetty in this context, compiling:(server/start.clj:13:3)
