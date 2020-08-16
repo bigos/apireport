ca4ab73d824f4ffa51466c24961aa0fed818d5c1
Jacek Podkanski <ruby.object@googlemail.com>
2020-08-17 00:21:41 +0100

    now clj -A:tasks starts the server

--- tasks/server/start.clj
+++ tasks/server/start.clj
@@ -1,17 +1,7 @@
 (ns server.start
-  (:require [ring.adapter.jetty]))
-
-;;; I want to run: clj -A:tasks
-;;; to start the server
-;;; similar to
-;; https://github.com/ring-clojure/ring/wiki/Getting-Started
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [apireport.core]))
 
 (defn -main []
   (println "task for starting server on port 3000")
-  (use 'ring.adapter.jetty)
-  (use 'apireport.core)
-  (run-jetty apireport.core/handler {:port 3000 :join? false})
-  )
-
-;;; i get the error
-;; Unable to resolve symbol: run-jetty in this context, compiling:(server/start.clj:13:3)
+  (run-jetty apireport.core/handler {:port 3000 :join? false}))
