23a14d6f3dc948c3715f2c551b38dd22db981e34
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2020-08-18 00:14:46 +0100

    refinement

--- task/server/start.clj
+++ task/server/start.clj
@@ -1,7 +1,7 @@
 (ns server.start
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [apireport.core]))
+            [apireport.core :refer [handler]]))
 
 (defn -main [& args]
   (println (str "task for starting server on port 3000 with args" args))
-  (run-jetty apireport.core/handler {:port 3000 :join? false}))
+  (run-jetty handler {:port 3000 :join? false}))
